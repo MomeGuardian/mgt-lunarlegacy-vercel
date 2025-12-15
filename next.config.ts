1fed4794ffd0786b28aace7c80ab0f74162037b0
ErenIsFree <2824889114@qq.com>
2025-12-16 04:42:11 +0800

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,13 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
-  reactCompiler: true,
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  async rewrites() {
+    return [
+      {
+        source: '/api/rpc',
+        destination: 'https://divine-orbital-dawn.solana-mainnet.quiknode.pro/b0b0db6c879f5ade13b4e2087c84f5d0c8f61739/'
+      },
+    ];
+  },
 };
 
 export default nextConfig;
