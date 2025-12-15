53b01aa59ac36b1738530a1352c949c6c84a555b
ErenIsFree <2824889114@qq.com>
2025-12-16 04:51:18 +0800

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -4,7 +4,6 @@ const nextConfig = {
     return [
       {
         source: '/api/rpc',
-        // 👇 目标地址：QuickNode 原生链接 + 正确的 b0b0 密钥
         destination: 'https://divine-orbital-dawn.solana-mainnet.quiknode.pro/b0b0db6c879f5ade13b4e2087c84f5d0c8f61739/',
       },
     ];
